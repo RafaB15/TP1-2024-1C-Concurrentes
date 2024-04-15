cc2ae2aabaf9d14c3d68a995efc48bd636750ad0
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-04-15 16:13:45 -0300

    Último format

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests {
-    use tp1_fork_join_108225::sites_information::sites_collection::SitesCollection;
+    use tp1_fork_join_108225::sites_information::{
+        parsing_error::ParsingError, sites_collection::SitesCollection,
+    };
 
     use serde_json::Value;
 
@@ -16,7 +18,7 @@ mod tests {
     #[test]
     fn we_can_load_multiple_sites() {
         let mut sites = SitesCollection::new();
-        let result = sites.load_sites("tests/testing_data/dos_archivos");
+        let result: Result<(), ParsingError> = sites.load_sites("tests/testing_data/dos_archivos");
         assert!(result.is_ok());
         let parsed: Value = sites.generate_json_information("108225");
         assert_eq!(parsed["padron"], "108225");
